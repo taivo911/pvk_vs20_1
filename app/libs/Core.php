5920a46d5bd9cf6a7b1cfe9448b907535b63eaaf
taivo911 <taivo.nurk@khk.ee>
2021-01-11 11:38:14 +0200

    Lingi andmed on ettevalmistatud kasutamiseks

--- app/libs/Core.php
+++ app/libs/Core.php
@@ -3,7 +3,6 @@
 
 class Core
 {
-
 // get url data
     /**
      * Core constructor.
@@ -16,7 +15,11 @@ class Core
     public function getUrl(){
         if(isset($_GET['url'])) {
             $url = $_GET['url'];
-            echo $url;
+            $url = rtrim($url, '/');
+            $url = htmlentities($url);
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
         }
     }
 }
